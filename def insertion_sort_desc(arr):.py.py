694680eb91034bf5c107c3717c32a10468afa75b
snevoji35690 <snevoji35690@ucumberlands.edu>
2025-05-17 19:15:52 -0500

    Add files via upload

---
+++ def insertion_sort_desc(arr):.py.py
@@ -0,0 +1,14 @@
+def insertion_sort_desc(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements that are less than key
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage:
+arr = [5, 2, 4, 6, 1, 3]
+insertion_sort_desc(arr)
+print("Sorted array (descending):", arr)
